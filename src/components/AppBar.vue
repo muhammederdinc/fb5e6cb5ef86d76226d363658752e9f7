318fe89480aed2b98722857668aee1077c805cd0
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-30 22:47:31 +0300

    created AppBar component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'AppBar',
+};
+</script>
+
+<template>
+  <v-app-bar
+    app
+    flat
+    color="blue darken-4"
+    height="100"
+    class="px-3"
+  >
+    <div class="white--text">
+      <h1>Otel</h1>
+      <span>Rezervasyon Sistemi</span>
+    </div>
+
+    <v-spacer />
+
+    <v-btn>Yeni Rezervasyon Yap</v-btn>
+  </v-app-bar>
+</template>
