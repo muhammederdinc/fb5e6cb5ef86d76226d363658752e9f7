1873d84a80e320091e47d7ee612e03121f4849a1
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-31 12:29:21 +0300

    added DatePicker in HotelDateSelection

--- src/components/ReservationSteps.vue
+++ src/components/ReservationSteps.vue
@@ -16,7 +16,7 @@ export default {
         :complete="step > 1"
         step="1"
       >
-        Name of step 1
+        Otel Ve Tarih Seçimi
       </v-stepper-step>
 
       <v-divider />
@@ -25,13 +25,13 @@ export default {
         :complete="step > 2"
         step="2"
       >
-        Name of step 2
+        Otel Tipi Ve Manzara Seçimi
       </v-stepper-step>
 
       <v-divider />
 
       <v-stepper-step step="3">
-        Name of step 3
+        Önizleme Ve Ödeme İşlemleri
       </v-stepper-step>
     </v-stepper-header>
 
--- src/views/newReservation/components/HotelDateSelection.vue
+++ src/views/newReservation/components/HotelDateSelection.vue
@@ -1,8 +1,17 @@
 <script>
 import { mapState } from 'vuex';
+import DatePicker from '../../../components/DatePicker';
 
 export default {
   name: 'HotelDateSelection',
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      formData: {},
+    };
+  },
   computed: {
     ...mapState(['hotelList']),
   },
@@ -14,10 +23,12 @@ export default {
     <v-row class="pa-10">
       <v-col cols="12">
         <v-autocomplete
+          v-model="formData.hotelName"
           :items="hotelList"
           outlined
           clearable
           dense flat
+          return-object
           item-value="id"
           item-text="hotel_name"
           prepend-icon="mdi-city"
@@ -28,19 +39,35 @@ export default {
       <v-col cols="12">
         <v-row>
           <v-col cols="3">
-            1
+            <date-picker
+              v-model="formData.start_date"
+              label="Giriş Tarihi"
+            />
           </v-col>
 
           <v-col cols="3">
-            1
+            <date-picker
+              v-model="formData.end_date"
+              label="Çıkış Tarihi"
+            />
           </v-col>
 
           <v-col cols="3">
-            1
+            <v-text-field
+              outlined
+              dense
+              type="number"
+              label="Yetişkin Sayısı"
+            />
           </v-col>
 
           <v-col cols="3">
-            1
+            <v-text-field
+              outlined
+              dense
+              type="number"
+              label="Çocuk Sayısı"
+            />
           </v-col>
         </v-row>
       </v-col>
