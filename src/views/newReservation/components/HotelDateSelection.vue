eed1a82a2ef58fc789aab595e426ec1d4424560c
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-31 11:15:20 +0300

    added HotelDateSelection component

---
+++ src/views/newReservation/components/HotelDateSelection.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'HotelDateSelection',
+};
+</script>
+
+<template>
+  <v-card outlined>
+    <v-row class="pa-10">
+      <v-col cols="12">
+        <v-autocomplete
+          solo
+          dense
+          flat
+          outlined
+          prepend-icon="mdi-city"
+        />
+      </v-col>
+
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="3">
+            1
+          </v-col>
+
+          <v-col cols="3">
+            1
+          </v-col>
+
+          <v-col cols="3">
+            1
+          </v-col>
+
+          <v-col cols="3">
+            1
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
--- src/views/newReservation/index.vue
+++ src/views/newReservation/index.vue
@@ -1,16 +1,28 @@
 <script>
 import ReservationSteps from '@/components/ReservationSteps';
+import HotelDateSelection from './components/HotelDateSelection';
 
 export default {
   name: 'NewReservation',
   components: {
     ReservationSteps,
+    HotelDateSelection,
   },
 };
 </script>
 
 <template>
-  <div>
-    <reservation-steps />
-  </div>
+  <reservation-steps>
+    <template v-slot:step1>
+      <hotel-date-selection />
+    </template>
+
+    <template v-slot:step2>
+      <h1>Here might be a page title</h1>
+    </template>
+
+    <template v-slot:step3>
+      <h1>Here might be a page title</h1>
+    </template>
+  </reservation-steps>
 </template>
