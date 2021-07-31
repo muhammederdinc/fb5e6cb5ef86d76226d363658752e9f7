d8588da6ef616b5fd6acd338a5fb4d47d06726cf
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-31 12:28:43 +0300

    created DatePicker component

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  name: 'DatePicker',
+  props: {
+    label: {
+      type: String,
+      default: 'Tarih',
+    },
+  },
+  data: () => ({
+    date: null,
+    isMenuVisible: false,
+  }),
+  methods: {
+    handleDateChange(value) {
+      this.isMenuVisible = false;
+
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
+
+<template>
+  <v-menu
+    v-model="isMenuVisible"
+    :nudge-right="20"
+    :nudge-top="20"
+    :close-on-content-click="false"
+    offset-y
+    min-width="auto"
+    transition="scale-transition"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        v-bind="$attrs"
+        v-model="date"
+        v-on="on"
+        :label="label"
+        dense
+        readonly
+        outlined
+        append-icon="mdi-calendar"
+      />
+    </template>
+
+    <v-date-picker
+      v-model="date"
+      @input="handleDateChange"
+    />
+  </v-menu>
+</template>
