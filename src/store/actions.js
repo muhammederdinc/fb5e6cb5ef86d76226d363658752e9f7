a62679f3b3372c6fdff7f60a90a1ff8d53cacfa5
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-31 12:39:23 +0300

    added hotelDetailList in state

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,4 +4,9 @@ export default {
       .then((response) => response.json())
       .then((data) => commit('setHotelList', data));
   },
+  fetchHotelDetailList({ commit }) {
+    fetch('https://5f6d939160cf97001641b049.mockapi.io/tkn/hotel-details')
+      .then((response) => response.json())
+      .then((data) => commit('setHotelDetailList', data));
+  },
 };
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,4 +2,7 @@ export default {
   setHotelList(state, hotelList) {
     state.hotelList = hotelList;
   },
+  setHotelDetailList(state, hotelDetailList) {
+    state.hotelDetailList = hotelDetailList;
+  },
 };
