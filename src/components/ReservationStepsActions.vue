87aa46c11563bd5a8838558171a2f1834f0504f1
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-08-01 12:42:17 +0300

    Created ReservationStepsActions component

---
+++ src/components/ReservationStepsActions.vue
@@ -0,0 +1,33 @@
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'ReservationStepsActions',
+  computed: {
+    ...mapState(['reservationStep']),
+  },
+};
+</script>
+
+<template>
+  <v-row justify="space-between">
+    <v-col cols="auto">
+      <v-btn
+        v-if="reservationStep > 1"
+        color="primary"
+        @click="$('previousStep')"
+      >
+        Geri
+      </v-btn>
+    </v-col>
+
+    <v-col cols="auto">
+      <v-btn
+        color="primary"
+        @click="$emit('nextStep')"
+      >
+        Kaydet Ve Devam Et
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
