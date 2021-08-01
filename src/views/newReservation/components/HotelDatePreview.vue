69855aac2bb4fbc9e0e1c18f6ccfdd472b5af8f5
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-08-01 18:50:47 +0300

    cretaed HotelDatePreview component

---
+++ src/views/newReservation/components/HotelDatePreview.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'HotelDatePreview',
+  props: {
+    reservation: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <v-card
+    color="grey lighten-3"
+    class="mb-5"
+    outlined
+  >
+    <v-card-title
+      primary-title
+    >
+      {{ reservation.hotel }}
+    </v-card-title>
+
+    <v-card-text>
+      <v-row class="d-flex">
+        <v-col class="d-flex" cols="auto">
+          <h4>Giriş Tarihi:</h4> {{ reservation.startDate }}
+        </v-col>
+
+        <v-col class="d-flex" cols="auto">
+          <h4>Çıkış Tarihi:</h4> {{ reservation.endDate }}
+        </v-col>
+
+        <v-col class="d-flex" cols="auto">
+          <h4>Yetişkin:</h4> {{ reservation.adult }}
+        </v-col>
+
+        <v-col class="d-flex" cols="auto">
+          <h4>Çocuk:</h4> {{ reservation.child }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
--- src/views/newReservation/components/HotelRoomAndViewSelection.vue
+++ src/views/newReservation/components/HotelRoomAndViewSelection.vue
@@ -1,10 +1,12 @@
 <script>
-import ReservationStepsActions from '@/components/ReservationStepsActions';
 import { mapGetters, mapState } from 'vuex';
+import ReservationStepsActions from '@/components/ReservationStepsActions';
+import HotelDatePreview from './HotelDatePreview';
 
 export default {
   name: 'HotelRoomAndViewSelection',
   components: {
+    HotelDatePreview,
     ReservationStepsActions,
   },
   computed: {
@@ -39,38 +41,10 @@ export default {
 
 <template>
   <div>
-    <v-card
+    <hotel-date-preview
       v-if="reservationInformation.hotel"
-      color="grey lighten-3"
-      class="mb-5"
-      outlined
-    >
-      <v-card-title
-        primary-title
-      >
-        {{ reservation.hotel }}
-      </v-card-title>
-
-      <v-card-text>
-        <v-row class="d-flex">
-          <v-col class="d-flex" cols="auto">
-            <h4>Giriş Tarihi:</h4> {{ reservation.startDate }}
-          </v-col>
-
-          <v-col class="d-flex" cols="auto">
-            <h4>Çıkış Tarihi:</h4> {{ reservation.endDate }}
-          </v-col>
-
-          <v-col class="d-flex" cols="auto">
-            <h4>Yetişkin:</h4> {{ reservation.adult }}
-          </v-col>
-
-          <v-col class="d-flex" cols="auto">
-            <h4>Çocuk:</h4> {{ reservation.child }}
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
+      :reservation="reservation"
+    />
 
     <v-row>
       <v-col cols="12">
