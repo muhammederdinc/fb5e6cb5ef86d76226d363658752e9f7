5d70c8f313d0da0611a5674dfe493c1c5fa8cbaa
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-08-03 10:06:39 +0300

    total price calculation

--- src/views/newReservation/components/HotelRoomAndViewSelection.vue
+++ src/views/newReservation/components/HotelRoomAndViewSelection.vue
@@ -42,6 +42,17 @@ export default {
         hotel: `${hotel.hotel_name} (${city})`,
       };
     },
+    accommodationPrice() {
+      const { room } = this.formData;
+      const dayCount = this.durationOfStay;
+
+      return room.price * dayCount;
+    },
+    durationOfStay() {
+      const { start_date: startDate, end_date: endDate } = this.reservationInformation;
+
+      return this.getNumberOfDays(startDate, endDate);
+    },
   },
   created() {
     const { room = null, roomScenic = null } = this.reservationInformation;
@@ -54,7 +65,12 @@ export default {
       const { room, roomScenic } = this.formData;
 
       if (room.id && roomScenic.id) {
-        this.goToNextStep(this.formData);
+        const params = {
+          ...this.formData,
+          price: this.getPrice(),
+        };
+
+        this.goToNextStep(params);
       } else {
         // to-do warning
       }
@@ -64,6 +80,22 @@ export default {
     },
     setSelectedRoomScenic(selectedRoomScenic) {
       this.formData.roomScenic = selectedRoomScenic;
+    },
+    getPrice() {
+      const { roomScenic } = this.formData;
+      const additionalPrice = (this.accommodationPrice * roomScenic.price_rate) / 100;
+      const result = this.accommodationPrice + additionalPrice;
+
+      return result;
+    },
+    getNumberOfDays(start, end) {
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+      const oneDay = 1000 * 60 * 60 * 24;
+      const diffInTime = endDate.getTime() - startDate.getTime();
+      const diffInDays = Math.round(diffInTime / oneDay);
+
+      return diffInDays;
     },
   },
 };
