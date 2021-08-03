15303fd8e94c41c5ec296774170417d7e1684f6e
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-08-03 19:44:32 +0300

    created AppConfirmDialog component

---
+++ src/components/AppConfirmDialog.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  name: 'AppConfirmDialog',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: 'mdi-alert-octagon',
+    },
+  },
+  data() {
+    return {
+      isDialogVisible: true,
+    };
+  },
+};
+</script>
+
+<template>
+  <v-dialog
+    v-model="isDialogVisible"
+    max-width="450"
+    persistent
+  >
+    <v-card>
+      <v-card-title class="d-flex justify-center">
+        <v-icon
+          v-text="icon"
+          x-large
+          color="warning"
+        />
+      </v-card-title>
+
+      <v-card-text
+        v-text="text"
+        class="body-1"
+      />
+
+      <v-card-actions>
+        <v-spacer />
+
+        <v-btn
+          text
+          color="error"
+          @click="$emit('cancel')"
+        >
+          İptal
+        </v-btn>
+
+        <v-btn
+          color="success"
+          class="white--text"
+          @click="$emit('confirm')"
+        >
+          Onayla
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
